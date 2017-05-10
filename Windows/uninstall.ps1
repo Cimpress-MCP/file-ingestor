102432ae8ec30d1ae021d55977483c957ac330df
David Purrington <dpurrington@cimpress.com>
2017-05-10 13:16:52 -0400

    Add uninstall script

--- Windows/uninstall.ps1
+++ Windows/uninstall.ps1
@@ -1,2 +1,2 @@
-schtasks /create /ru "NT AUTHORITY\LOCALSERVICE" /sc MINUTE /tn file-ingestor /tr c:\ProgramData\file-ingestor\ingest-files.bat
-start .\python-3.6.1-amd64-webinstall.exe -ArgumentList /uninstall -wait
+schtasks /delete /tn file-ingestor /F
+rmdir \ProgramData\file-ingestor -Recurse -Force
